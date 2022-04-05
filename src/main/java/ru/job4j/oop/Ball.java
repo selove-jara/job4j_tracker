95e3ac361f9d085dff9432a44bbd184fea6ccadf
Andrey Morozov <selove.jara@gmail.com>
2022-04-05 17:11:03 +0400

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Ball {
+
+    public Ball tryRun(boolean condition) {
+        boolean result = false;
+        if (condition) {
+            System.out.println("Kolobok RIP");
+        }else {
+            System.out.println("Kolobok run");
+        }
+        return null;
+    }
+}
---
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class BallStory {
+
+    public static void main(String[] args) {
+        Ball kolobok = new Ball();
+        Hare oblique  = new Hare();
+        Wolf grey  = new Wolf();
+        Fox sly  = new Fox();
+        oblique.tryEat(kolobok.tryRun(false));
+        grey.tryEat(kolobok.tryRun(false));
+        sly .tryEat(kolobok.tryRun(true));
+    }
+}
