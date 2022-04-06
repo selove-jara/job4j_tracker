6ba26867c1611c4bd03cbb6233c4c2ffa2177431
Andrey Morozov <selove.jara@gmail.com>
2022-04-06 11:23:21 +0400

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error (boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error666 = new Error(true, 666, "Перезагрузи компьютерте, обычно помогает.");
+        error666.printInfo();
+        Error error400 = new Error(false, 400, "Плохой запрос.");
+        error400.printInfo();
+        Error error404 = new Error(true, 404, "Запрошенный материал не найден.");
+        error404.printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println("Активна: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+}
+
