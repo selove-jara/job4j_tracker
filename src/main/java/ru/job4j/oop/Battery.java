40bb474529ba2c30d3fabd9aeaca58f83535591f
Andrey Morozov <selove.jara@gmail.com>
2022-04-07 11:29:09 +0400

    1.8. Состояние объекта [#167388]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -9,7 +9,7 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        another.load = another.load + this.load ;
+        another.load = another.load + this.load;
         this.load = 0;
     }
 
