af3c7227ac8884e8d97d6788aa1ae0d740a95649
Andrey Morozov <selove.jara@gmail.com>
2022-04-12 17:33:28 +0400

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,18 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +30,18 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Имя предмета: " + name);
+        System.out.println("Номер предмета: " + id);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item("GeForce RTX 3090 GamingPro");
+        Item atom = new Item(4715084, "GeForce RTX 3090 GamingPro");
+        first.printInfo();
+        atom.printInfo();
     }
 }
