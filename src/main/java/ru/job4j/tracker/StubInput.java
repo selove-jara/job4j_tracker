922631ce839d20ddc8684f847dd59f4f60776548
Andrey Morozov <selove.jara@gmail.com>
2022-04-29 15:09:05 +0400

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117 #294737]

--- src/main/java/ru/job4j/tracker/StubInput.java
+++ src/main/java/ru/job4j/tracker/StubInput.java
@@ -8,18 +8,6 @@ public class StubInput implements Input {
         this.answers = answers;
     }
 
-    private boolean isNumber(String value) {
-        boolean rsl = true;
-        char[] check = value.toCharArray();
-        for (char num : check) {
-            if (num < 48 || num > 57) {
-                rsl = false;
-                break;
-            }
-        }
-        return rsl;
-    }
-
     @Override
     public String askStr(String question) {
         return answers[position++];
@@ -27,17 +15,6 @@ public class StubInput implements Input {
 
     @Override
     public int askInt(String question) {
-        boolean invalid = true;
-        int value = -1;
-        do {
-            String rsl = askStr(question);
-            if (!isNumber(rsl)) {
-                System.out.println("Please enter validate data again.");
-                continue;
-            }
-            value = Integer.parseInt(rsl);
-            invalid = false;
-        } while (invalid);
-        return value;
+        return Integer.parseInt(askStr(question));
     }
 }
