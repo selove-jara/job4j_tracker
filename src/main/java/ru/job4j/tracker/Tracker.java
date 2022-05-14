90a4d62d8bb1e45a8b134c442f929567bdca4e12
Andrey Morozov <selove.jara@gmail.com>
2022-05-14 11:16:32 +0400

    6. Изменить программу Tracker из 2-го модуля [#10039 #300800]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -41,7 +41,7 @@ public class Tracker {
                 rsl.add(item);
             }
         }
-        return List.copyOf(rsl);
+        return rsl;
     }
 
     public boolean replace(int id, Item item) {
