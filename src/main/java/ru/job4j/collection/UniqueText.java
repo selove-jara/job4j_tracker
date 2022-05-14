4d2b6085732f1a5c4942b35ebb867d92334312e5
Andrey Morozov <selove.jara@gmail.com>
2022-05-14 17:16:32 +0400

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String s : origin) {
+            check.add(s);
+        }
+        for (String element : text) {
+            if (!check.contains(element)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
