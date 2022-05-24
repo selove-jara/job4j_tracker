70d54010e07e3a8a7637762f3bb0373505ae0dea
Andrey Morozov <selove.jara@gmail.com>
2022-05-24 17:43:56 +0400

    3. Ссылки на методы [#70608]

---
+++ src/main/java/ru/job4j/function/RefMethod.java
@@ -0,0 +1,27 @@
+package ru.job4j.function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList(
+                "Ivan",
+                "Petr Arsentev"
+        );
+        Consumer<String> out = (name) -> cutOut(name);
+        names.forEach(out);
+
+        Consumer<String> out1 = RefMethod::cutOut;
+        names.forEach(out1);
+    }
+
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
+            System.out.println(value.substring(0, 10) + "..");
+        } else {
+            System.out.println(value);
+        }
+    }
+}
