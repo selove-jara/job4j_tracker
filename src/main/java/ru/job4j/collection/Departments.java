b526445215db2f25af41275269292e3ff43f3be3
Andrey Morozov <selove.jara@gmail.com>
2022-05-30 11:38:32 +0400

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -9,12 +9,8 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (!start.isEmpty()) {
-                    el = start + "/" + el;
-                }
-                tmp.add(el);
-                start = el;
-
+                start += "".equals(start) ? el : "/" + el;
+                tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);
