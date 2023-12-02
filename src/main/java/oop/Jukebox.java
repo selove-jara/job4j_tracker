6a362b0036e54bd34f12ad7d8d43ee380525da73
Andrey Morozov <selove.jara@gmail.com>
2023-12-02 10:06:41 +0400

    1.3. Вызов метода с аргументами. [#166086 #282237]

---
+++ src/main/java/oop/Jukebox.java
@@ -0,0 +1,23 @@
+package oop;
+
+public class Jukebox {
+    public void music(int position) {
+        switch (position) {
+            case 1:
+                System.out.println("Пусть бегут неуклюже");
+                break;
+            case 2:
+                System.out.println("Спокойной ночи");
+                break;
+            default:
+                System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox song = new Jukebox();
+        song.music(1);
+        song.music(2);
+        song.music(3);
+    }
+}
