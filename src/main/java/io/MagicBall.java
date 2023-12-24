4045afeaa2d76455031d3d7bb14ee783fcdd98e1
Andrey Morozov <selove.jara@gmail.com>
2023-12-24 16:46:51 +0400

    2.0. Чтение из консоли. Класс Scanner. [#179514 #290927]

---
+++ src/main/java/io/MagicBall.java
@@ -0,0 +1,20 @@
+package io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Задайте закрытый вопрос. -");
+        input.nextLine();
+        int answer = new Random().nextInt(3);
+        if (answer == 0) {
+            System.out.println("Да");
+        } else if (answer == 1) {
+            System.out.println("Нет");
+        } else {
+            System.out.println("Может быть");
+        }
+    }
+}
