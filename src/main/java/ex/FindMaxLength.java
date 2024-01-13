5df406a01eb1c02ef0313ab57af903b05d734155
Andrey Morozov <selove.jara@gmail.com>
2024-01-13 18:22:34 +0400

    0.1. java.lang.NullPointerException [#219364 #293532]

---
+++ src/main/java/ex/FindMaxLength.java
@@ -0,0 +1,17 @@
+package ex;
+
+public class FindMaxLength {
+    public static void main(String[] args) {
+        String[] shops = {"Ebay", null, "Amazon", null, "Ozon"};
+        int max = 0;
+        for (int index = 0; index < shops.length; index++) {
+            if (shops[index] != null) {
+                String element = shops[index];
+                if (element.length() > max) {
+                    max = element.length();
+                }
+            }
+            System.out.println("Max length : " + max);
+        }
+    }
+}
