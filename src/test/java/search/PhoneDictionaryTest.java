3dc7cb56c10fc726e1f7f1c1f6895fd2e7620908
Andrey Morozov <selove.jara@gmail.com>
2024-03-06 21:46:30 +0400

    4. Телефонный справочник на базе ArrayList [#41598 #300130]

---
+++ src/test/java/search/PhoneDictionaryTest.java
@@ -0,0 +1,19 @@
+package search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+}
