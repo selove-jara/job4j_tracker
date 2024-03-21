af0628a90c1c760e5da19d598560124c71cc77ea
selov <selove.jara@gmail.com>
2024-03-21 17:07:44 +0400

    2.1. Лямбда блок [#249208 #304615]

--- src/main/java/lambda/LambdaUsage.java
+++ src/main/java/lambda/LambdaUsage.java
@@ -7,17 +7,15 @@ import java.util.List;
 public class LambdaUsage {
 
     public static void main(String[] args) {
-        List<String> strings = Arrays.asList("eeeee", "a",  "ccc", "dddd", "bb");
+        List<String> strings = Arrays.asList("eeeee", "a", "ccc", "dddd", "bb");
         Comparator<String> comparator = (left, right) -> {
             System.out.println("compare - " + right + " : " + left);
             return Integer.compare(right.length(), left.length());
         };
 
-            strings.sort(comparator);
-            for (String string : strings) {
-                System.out.println(string);
-            }
-
-
+        strings.sort(comparator);
+        for (String string : strings) {
+            System.out.println(string);
+        }
     }
 }
