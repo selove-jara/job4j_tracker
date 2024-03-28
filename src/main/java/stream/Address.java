300d67ffedb65e305c092279fc37ad22120ce92c
selov <selove.jara@gmail.com>
2024-03-28 19:29:25 +0400

    7. Уникальность элементов и сортировка. [#110225 #305162]

--- src/main/java/stream/Address.java
+++ src/main/java/stream/Address.java
@@ -37,4 +37,36 @@ public class Address {
     public int hashCode() {
         return Objects.hash(city, street, home, apartment);
     }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHome() {
+        return home;
+    }
+
+    public void setHome(int home) {
+        this.home = home;
+    }
+
+    public int getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(int apartment) {
+        this.apartment = apartment;
+    }
 }
--- src/main/java/stream/Profiles.java
+++ src/main/java/stream/Profiles.java
@@ -4,10 +4,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(profile -> profile.getAddress())
                 .collect(Collectors.toList());
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .sorted((o1, o2) -> o1.getCity().compareTo(o2.getCity()))
+                .distinct()
+                .collect(Collectors.toList());
+
     }
 }
