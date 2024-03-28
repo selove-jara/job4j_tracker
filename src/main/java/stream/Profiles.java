c677f9d72cea5e8a3d04d922891d5421d682bba5
selov <selove.jara@gmail.com>
2024-03-28 18:04:33 +0400

    6. Список адресов [#110062 #305136]

---
+++ src/main/java/stream/Profiles.java
@@ -0,0 +1,13 @@
+package stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .collect(Collectors.toList());
+    }
+}
